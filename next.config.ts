204cb02934487114fc82bcb48bebf692d5f82699
viniciorm <marcos.reyes.m@gmail.com>
2026-02-20 11:13:58 -0300

    Fix: Reverted basePath to restore root access on Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/app-club-lectura',
-  // Si vas a usar imágenes con el basePath, asegúrate de actualizar las rutas si es necesario
+  /* config options here */
 };
 
 export default nextConfig;
