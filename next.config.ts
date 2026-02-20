88642b601db7ef0c043aeb2e4ed796ad8db8bb33
viniciorm <marcos.reyes.m@gmail.com>
2026-02-20 11:10:08 -0300

    Update: Configured basePath to /app-club-lectura

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/app-club-lectura',
+  // Si vas a usar imágenes con el basePath, asegúrate de actualizar las rutas si es necesario
 };
 
 export default nextConfig;
